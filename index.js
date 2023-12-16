330b43d93ce9685e6258b72517e57c6ae0f6ce4a
omutucat <57890334+omutucat@users.noreply.github.com>
2023-12-17 00:05:08 +0900

    code formatting

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { token } = require('./config.json');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
-	console.log(`${client.user.tag}でログインしました。`);
+  console.log(`${client.user.tag}でログインしました。`);
 });
 
 client.login(token);
@@ -14,32 +14,37 @@ client.login(token);
 client.commands = new Collection();
 
 const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`${filePath} に必要な "data" か "execute" がありません。`);
-	}
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(`${filePath} に必要な "data" か "execute" がありません。`);
+  }
 }
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-	const command = interaction.client.commands.get(interaction.commandName);
+  const command = interaction.client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`${interaction.commandName} が見つかりません。`);
-		return;
-	}
+  if (!command) {
+    console.error(`${interaction.commandName} が見つかりません。`);
+    return;
+  }
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'エラーが発生しました。', ephemeral: true });
-	}
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: 'エラーが発生しました。',
+      ephemeral: true,
+    });
+  }
 });
